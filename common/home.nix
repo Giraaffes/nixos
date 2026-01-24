ca675d4cbfd959190d1cbaec478b76458d1b973f
Marcus Færk Henriksen <marcusfaerk@gmail.com>
2026-01-24 12:15:48 +0100

    Fixed wifi usb on marcus-mor, fixed duplicate VSCode entries

--- common/home.nix
+++ common/home.nix
@@ -24,9 +24,8 @@
       firefox slack # Default applications
       vscode nodejs pnpm dbeaver-bin postgresql # Dev environment
       prisma-engines_7 openssl # For Prisma
-      gimp2 # Others
+      gimp2 jq # Others
 
-      nixd # Nix language server
       ventoy-full-qt # For Nix .iso installer
     ];
 
@@ -36,6 +35,15 @@
       PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines_7}/lib/libquery_engine.node";
       PRISMA_FMT_BINARY = "${pkgs.prisma-engines_7}/bin/prisma-fmt";
       LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.openssl ]}";
+    };
+
+    xdg.desktopEntries.code-url-handler = {
+      name = "Visual Studio Code - URL Handler";
+      comment = "Code Editing. Redefined.";
+      exec = "code --open-url %U";
+      mimeType = [ "x-scheme-handler/vscode" ];
+      noDisplay = true;
+      icon = "vscode";
     };
 
     home.stateVersion = "25.11";
--- hosts/marcus-mor/configuration.nix
+++ hosts/marcus-mor/configuration.nix
@@ -13,9 +13,14 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8821cu
   ];
+
+  services.udev.extraRules = ''
+    ACTION=="add", ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", RUN+="${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 0bda -p 1a2b -K"
+  '';
   
   environment.systemPackages = with pkgs; [
     usbutils
+    usb-modeswitch
   ];
 
   home-manager.users.marcus.home.shellAliases = {
